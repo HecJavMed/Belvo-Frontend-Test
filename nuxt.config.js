1238059c99da3a14a0be5a003a29e7e0bd479353
HecJMedina <hecjmh@gmail.com>
2024-11-17 17:21:12 -0600

    Update base URL for Axios and modify start script in package.json

--- nuxt.config.js
+++ nuxt.config.js
@@ -47,7 +47,7 @@ export default {
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: 'http://localhost:5000'
+    baseURL: 'https://belvo-backend-test-production.up.railway.app'
   },
 
   // Vuetify module configuration: https://go.nuxtjs.dev/config-vuetify
